57e6e2199e019573ddc06880467db1e13a8ce0a1
Andy Maesen <maesenandy@gmail.com>
2018-03-10 09:48:32 +0100

    Moved combat encounter method to the Encounter class

--- RealmRpgBot/Bot/Commands/GeneralActionCommands.cs
+++ RealmRpgBot/Bot/Commands/GeneralActionCommands.cs
@@ -447,81 +447,10 @@ namespace RealmRpgBot.Bot.Commands
 				{
 					var encounterId = location.Encounters?.GetRandomEntry();
 					var encounter = await session.LoadAsync<Encounter>(encounterId);
-
-					// TODO: Put in a method somewhere else to make more concise
+					
 					if (encounter?.EncounterType == Encounter.EncounterTypes.Enemy)
 					{
-						var templates = (await session.LoadAsync<EncounterTemplate>(encounter.Templates)).Values
-							.Where(t => t.LevelRangeMin >= player.Level && player.Level <= t.LevelRangeMin || t.AdjustToPlayerLevel)
-							.ToList();
-
-						if (templates.Count == 0)
-						{
-							await c.RespondAsync("Nothing to see here");
-							await c.ConfirmMessage();
-							return;
-						}
-
-						var template = templates.GetRandomEntry();
-
-						var enemy = new Enemy(template, player.Level);
-
-						var encounterEmbed = new DiscordEmbedBuilder()
-							.WithTitle($"Encounter with {enemy.Name}");
-
-						var body = new System.Text.StringBuilder();
-						body.AppendLine($"{c.User.Mention} has encountered a lvl{enemy.Level} {enemy.Name} with {enemy.HpCurrent}hp.");
-
-						var combat = new AutoBattle(player, enemy, c);
-						await combat.StartCombatAsync();
-
-						var result = await combat.GetCombatResultAsync();
-
-						string xpMsg = string.Empty;
-						switch (result.Outcome)
-						{
-							case CombatOutcome.Attacker:
-								{
-									var xpGain = encounter.GetActualXpReward(player.Level, enemy.Level);
-									await player.AddXpAsync(xpGain, c);
-									xpMsg = $"Gained {xpGain}xp";
-									break;
-								}
-							case CombatOutcome.Defender:
-								{
-									var xpLost = await player.SetFaintedAsync();
-									xpMsg = $"Lost {xpLost}xp.";
-									break;
-								}
-							case CombatOutcome.Tie:
-								{
-									var xpGain = encounter.GetActualXpReward(player.Level, enemy.Level);
-									await player.AddXpAsync(xpGain, c);
-									var xpLost = await player.SetFaintedAsync();
-									xpMsg = $"Gained {xpGain}xp, but lost {xpLost}xp.";
-									break;
-								}
-						}
-
-						encounterEmbed.WithFooter(xpMsg);
-
-						body.AppendLine();
-						body.AppendLine("*Last lines of Combat Log*");
-						body.AppendLine("*...*");
-
-						foreach (var line in combat.CombatLog.Skip(System.Math.Max(0, combat.CombatLog.Count - 3)))
-						{
-							body.AppendLine(line);
-						}
-
-						body.AppendLine();
-						body.AppendLine(result.Message);
-
-						encounterEmbed.WithDescription(body.ToString());
-
-						await player.SetActionAsync(Constants.ACTION_REST, "Recovering from combat", System.TimeSpan.FromMinutes(1));
-
-						await c.RespondAsync(embed: encounterEmbed.Build());
+						await encounter.DoBattleEncounter(session, c, player);
 					}
 				}
 
--- RealmRpgBot/Models/Encounters/Encounter.cs
+++ RealmRpgBot/Models/Encounters/Encounter.cs
@@ -1,6 +1,15 @@
 ﻿namespace RealmRpgBot.Models.Encounters
 {
 	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using DSharpPlus.CommandsNext;
+	using DSharpPlus.Entities;
+	using Raven.Client.Documents.Session;
+
+	using Character;
+	using Combat;
 
 	public class Encounter
 	{
@@ -11,6 +20,88 @@
 		public int XpReward { get; set; }
 
 		public int GetActualXpReward(int playerLevel, int enemyLevel) => XpReward / 100 * Rpg.GetGainedXpModifier(playerLevel, enemyLevel);
+
+		/// <summary>
+		/// Do automatic battle encounter
+		/// </summary>
+		/// <param name="session">Database session. One should be open anyway to get encounter info</param>
+		/// <param name="cmdCtx">Discord command context</param>
+		/// <param name="player">Player doing the encounter</param>
+		/// <returns></returns>
+		public async Task DoBattleEncounter(IAsyncDocumentSession session, CommandContext cmdCtx, Player player)
+		{
+			var templates = (await session.LoadAsync<EncounterTemplate>(Templates)).Values
+							.Where(t => t.LevelRangeMin >= player.Level && player.Level <= t.LevelRangeMin || t.AdjustToPlayerLevel)
+							.ToList();
+
+			if (templates.Count == 0)
+			{
+				await cmdCtx.RespondAsync("Nothing to see here");
+				await cmdCtx.ConfirmMessage();
+				return;
+			}
+
+			var template = templates.GetRandomEntry();
+
+			var enemy = new Enemy(template, player.Level);
+
+			var encounterEmbed = new DiscordEmbedBuilder()
+				.WithTitle($"Encounter with {enemy.Name}");
+
+			var body = new System.Text.StringBuilder();
+			body.AppendLine($"{cmdCtx.User.Mention} has encountered a lvl{enemy.Level} {enemy.Name} with {enemy.HpCurrent}hp.");
+
+			var combat = new AutoBattle(player, enemy, cmdCtx);
+			await combat.StartCombatAsync();
+
+			var result = await combat.GetCombatResultAsync();
+
+			string xpMsg = string.Empty;
+			switch (result.Outcome)
+			{
+				case CombatOutcome.Attacker:
+					{
+						var xpGain = GetActualXpReward(player.Level, enemy.Level);
+						await player.AddXpAsync(xpGain, cmdCtx);
+						xpMsg = $"Gained {xpGain}xp";
+						break;
+					}
+				case CombatOutcome.Defender:
+					{
+						var xpLost = await player.SetFaintedAsync();
+						xpMsg = $"Lost {xpLost}xp.";
+						break;
+					}
+				case CombatOutcome.Tie:
+					{
+						var xpGain = GetActualXpReward(player.Level, enemy.Level);
+						await player.AddXpAsync(xpGain, cmdCtx);
+						var xpLost = await player.SetFaintedAsync();
+						xpMsg = $"Gained {xpGain}xp, but lost {xpLost}xp.";
+						break;
+					}
+			}
+
+			encounterEmbed.WithFooter(xpMsg);
+
+			body.AppendLine();
+			body.AppendLine("*Last lines of Combat Log*");
+			body.AppendLine("*...*");
+
+			foreach (var line in combat.CombatLog.Skip(System.Math.Max(0, combat.CombatLog.Count - 3)))
+			{
+				body.AppendLine(line);
+			}
+
+			body.AppendLine();
+			body.AppendLine(result.Message);
+
+			encounterEmbed.WithDescription(body.ToString());
+
+			await player.SetActionAsync(Constants.ACTION_REST, "Recovering from combat", System.TimeSpan.FromMinutes(1));
+
+			await cmdCtx.RespondAsync(embed: encounterEmbed.Build());
+		}
 
 		public enum EncounterTypes
 		{
